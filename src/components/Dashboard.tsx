6620a812375762dcc2260b16d44f423a010a0729
Sean Brown <seanbrown82@hotmail.com>
2022-12-09 15:22:19 +0000

    Feat: Create dashboard layout

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Container } from "react-bootstrap"
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <Container>
+      <h2>Dashboard</h2>
+      <Container id="dashboardTicketContainer">
+        <h3>New Tickets</h3>
+        <div>New Tickets to be displayed here</div>
+        <Link to="#">View all tickets</Link>
+      </Container>
+      <Container id="dashboardProjectContainer">
+        <h3>Projects</h3>
+        <div>Projects to be displayed here</div>
+        <Link to="#">View all projects</Link>
+      </Container>
+      
+    </Container>
   )
 }
 
