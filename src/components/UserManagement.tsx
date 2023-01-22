d9f19a4c0ed2d025a82119da0752ada6764f9d6b
Sean Brown <seanbrown82@hotmail.com>
2023-01-22 19:14:44 +0000

    Feat: Add ability to assign user roles

--- src/components/UserManagement.tsx
+++ src/components/UserManagement.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { useState, useEffect, FormEvent } from "react";
+import { collection, getDocs, doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { IUser } from "../ts/interfaces/interfaces";
+import { Form, Button } from "react-bootstrap"
 
 const UserManagement = ({ user }: any) => {
   const [users, setUsers] = useState<Array<IUser>>([])
@@ -13,6 +14,24 @@ const UserManagement = ({ user }: any) => {
     setUsers(downloadedUsers)
   }
 
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const selectedUser = (document.getElementById("userSelect") as HTMLInputElement).value
+    const selectedRole =(document.getElementById("roleSelect") as HTMLInputElement).value
+    console.log(selectedUser, selectedRole)
+    updateUserRole(selectedUser, selectedRole)
+
+
+  }
+
+  async function updateUserRole(id: string, newRole: string) {
+    const docRef = doc(db, "users", id)
+    await updateDoc(docRef, {
+      role: newRole
+    })
+
+  }
+
   useEffect(() => {
     getAllUsers()
 
@@ -20,7 +39,25 @@ const UserManagement = ({ user }: any) => {
 
   return (
     <div>
-      {users && users.map(user => <p>Name: {user.name}, Role: {user.role}</p>)}
+      <Form action="#" method="#" onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Select User</Form.Label>
+          <Form.Select id="userSelect">
+          {users && users.map(user => <option value={user.id}>{user.name}</option>)}
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Select Role</Form.Label>
+          <Form.Select id="roleSelect">
+            <option value="user">User</option>
+            <option value="developer">Developer</option>
+            <option value="projectManager">Project Manager</option>
+            <option value="administrator">Administrator</option>
+          </Form.Select>
+        </Form.Group>
+        <Button type="submit" className="btn btn-primary">Update Role</Button>
+      </Form>
     </div>
   )
 }
